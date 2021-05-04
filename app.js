660f0b2b9b796d390a087118bab61c3ff363b44b
Walter Huang <autumnhao@gmail.com>
2021-05-04 13:45:52 -0700

    Add BootStrap5 library to do some style work.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const CampGround = require('./models/campGround');
-const methodOverride = require('method-override');
-const { DH_CHECK_P_NOT_SAFE_PRIME } = require('constants');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const CampGround = require("./models/campGround");
+const methodOverride = require("method-override");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,67 +13,69 @@ mongoose.connect(process.env.ATLAS_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
-  useCreateIndex: true
+  useCreateIndex: true,
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection Error:'));
-db.once('open', () => {
-  console.log('Atlas Database Connected!');
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once("open", () => {
+  console.log("Atlas Database Connected!");
 });
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.use(methodOverride("_method"));
 
-app.get('/', (req, res) => {
-  res.render('home');
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get('/campgrounds', async (req, res) => {
+app.get("/campgrounds", async (req, res) => {
   const campgrounds = await CampGround.find({});
-  res.render('campgrounds/index', { campgrounds });
+  res.render("campgrounds/index", { campgrounds });
 });
 
-app.get('/campgrounds/new', (req, res) => {
-  res.render('campgrounds/new')
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
 });
 
-app.post('/campgrounds', async (req, res) => {
+app.post("/campgrounds", async (req, res) => {
   const campground = new CampGround(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground.id}`);
 });
 
-
-app.get('/campgrounds/:id', async (req, res) => {
+app.get("/campgrounds/:id", async (req, res) => {
   const { id } = req.params;
-  const campground = await CampGround.findById(id)
-  res.render('campgrounds/show', { campground });
+  const campground = await CampGround.findById(id);
+  res.render("campgrounds/show", { campground });
 });
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get("/campgrounds/:id/edit", async (req, res) => {
   const { id } = req.params;
-  const campground = await CampGround.findById(id)
-  res.render('campgrounds/edit', { campground });
+  const campground = await CampGround.findById(id);
+  res.render("campgrounds/edit", { campground });
 });
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put("/campgrounds/:id", async (req, res) => {
   const { id } = req.params;
-  const campground = await CampGround.findByIdAndUpdate(id, { ...req.body.campground });
+  const campground = await CampGround.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
   res.redirect(`/campgrounds/${campground.id}`);
 });
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete("/campgrounds/:id", async (req, res) => {
   const { id } = req.params;
   await CampGround.findByIdAndDelete(id);
-  res.redirect('/campgrounds');
+  res.redirect("/campgrounds");
 });
 
 app.listen(port, () => {
-  console.log('Start listening on port'.toUpperCase(), port);
+  console.log("Start listening on port".toUpperCase(), port);
 });
-
