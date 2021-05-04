4e76175c29348641d1da0c3a24adbf88c56c0b75
Walter Huang <autumnhao@gmail.com>
2021-05-04 11:36:16 -0700

    Implement the delete function for user.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const CampGround = require('./models/campGround');
+const methodOverride = require('method-override');
+const { DH_CHECK_P_NOT_SAFE_PRIME } = require('constants');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -23,10 +25,9 @@ db.once('open', () => {
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -35,7 +36,41 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await CampGround.find({});
   res.render('campgrounds/index', { campgrounds });
-  console.log(campgrounds);
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new')
+});
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new CampGround(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground.id}`);
+});
+
+
+app.get('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await CampGround.findById(id)
+  res.render('campgrounds/show', { campground });
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  const campground = await CampGround.findById(id)
+  res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await CampGround.findByIdAndUpdate(id, { ...req.body.campground });
+  res.redirect(`/campgrounds/${campground.id}`);
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  await CampGround.findByIdAndDelete(id);
+  res.redirect('/campgrounds');
 });
 
 app.listen(port, () => {
