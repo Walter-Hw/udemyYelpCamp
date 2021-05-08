ece77f64aa3f1262267ff8a3cd44e127bdf88474
Walter Huang <autumnhao@gmail.com>
2021-05-08 12:17:49 -0700

    Fix bugs and modidy prettier configuration.

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-const ejsMate = require("ejs-mate");
-const session = require("express-session");
-const flash = require("connect-flash");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
 
-const { campgroundSchema, reviewSchema } = require("./schemaValidation");
-const Review = require("./models/review");
-const methodOverride = require("method-override");
-const CampGround = require("./models/campGround");
+const { campgroundSchema, reviewSchema } = require('./schemaValidation');
+const Review = require('./models/review');
+const methodOverride = require('method-override');
+const CampGround = require('./models/campGround');
 
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -25,15 +25,15 @@ mongoose.connect(process.env.ATLAS_URL, {
 });
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "Connection Error:"));
-db.once("open", () => {
-  console.log("Atlas Database Connected Successfully!!!".toUpperCase());
+db.on('error', console.error.bind(console, 'Connection Error:'));
+db.once('open', () => {
+  console.log('Atlas Database Connected Successfully!!!'.toUpperCase());
 });
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 app.use(flash());
 
 const sessionConfig = {
@@ -48,35 +48,35 @@ const sessionConfig = {
 };
 app.use(session(sessionConfig));
 
-app.engine("ejs", ejsMate);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use((req, res, next) => {
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
   next();
 });
 
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
 
-app.get("/", (req, res) => {
-  res.render("home");
+app.get('/', (req, res) => {
+  res.render('home');
 });
 
-app.all("*", (req, res, next) => {
-  next(new ExpressError("Page not found", 404));
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page not found', 404));
 });
 
 app.use((err, req, res, next) => {
   const { statusCode = 500 } = err;
   if (!err.message) {
-    err.message = "OHOH! Error occurs!";
+    err.message = 'OHOH! Error occurs!';
   }
-  res.status(statusCode).render("err", { err });
+  res.status(statusCode).render('err', { err });
 });
 
 app.listen(port, () => {
-  console.log("Start listening on port".toUpperCase(), port);
+  console.log('Start listening on port'.toUpperCase(), port);
 });
