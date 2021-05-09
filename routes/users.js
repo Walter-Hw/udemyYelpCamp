0bf89ac34b3121aee981c6246df811483c1ba5af
Walter Huang <autumnhao@gmail.com>
2021-05-08 19:39:44 -0700

    Finish the user register part and ready to implement the user login part.

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,27 @@
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
 
 router.get('/register', (req, res) => {
   res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
-  res.send(req.body);
-});
+router.post(
+  '/register',
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.flash('success', `Welcome to YelpCamp ${_.capitalize(username)}`);
+      res.redirect('/campgrounds');
+    } catch (err) {
+      req.flash('error', err.message);
+      res.redirect('/register');
+    }
+  })
+);
 
 module.exports = router;
